0be14c80e61bf3f80515c17ec852f70736936214
ivan <sergeevi4ivan@gmail.com>
2024-03-16 16:25:04 +0300

    first commit

---
+++ src/test/java/ru/panas/APizzagateway/APizzaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.panas.APizzagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APizzaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
